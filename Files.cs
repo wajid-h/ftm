bf3a281544b86b52e5c7beb0543f436af45ec768
Wajid Hussain <wajidshar60@gmail.com>
2025-02-03 19:44:26 +0500

    gignore

--- Files.cs
+++ Files.cs
@@ -1,37 +1,66 @@
-using System.Text;
+#pragma warning disable IDE0130
 
-namespace FTM.FileController
+using System.Runtime.CompilerServices;
+
+namespace FTM.FileControllers
 {
+#pragma warning restore
+
+
     public class FileMover
     {
         public enum MoveType { File, Directory }
-        public static bool MoveFile(string file, string destination, bool overwrite = false) =>  Move(file  , destination, MoveType.File , overwrite) ;
-        public static void MoveDir(string file, string destination) => Move(file, destination , MoveType.Directory) ; 
+        internal enum PutType { Copy, Move }
 
-        static internal bool Move(string target, string destination, MoveType type,  bool overwrite = false  )
-        {   
-            try 
-            {   
-                bool isDirectory   = type == MoveType.Directory ;
-                bool exists =    !isDirectory ?  File.Exists(target) :  Directory.Exists(target) ; 
-                
+        public static bool MoveFile(string path, string destination, bool overwrite = false) => Put(path, destination, MoveType.File, overwrite);
+        public static bool MoveDirectory(string path, string destination) => Put(path, destination, MoveType.Directory);
+
+        static internal bool Put(string target, string destination, MoveType type, bool overwrite = false, PutType put = PutType.Move)
+        {
+            try
+            {
+                bool isDirectory = type == MoveType.Directory;
+                bool exists = !isDirectory ? File.Exists(target) : Directory.Exists(target);
+
                 if (!exists) return default;
 
-                if(isDirectory)
-                Directory.Move(target, destination);
-                else  File.Move(target, destination , overwrite) ;
-                
+                if (put == PutType.Move)
+                {
+                    if (isDirectory) Directory.Move(target, destination);
+                    else File.Move(target, destination, overwrite);
+                }
+                else
+                {
+                    if (isDirectory) CopyDirectory(target, destination, true); // TODO   copy dir call;
+                    else File.Copy(target, destination, overwrite);
+                }
                 return true;
             }
-            catch (UnauthorizedAccessException) { return default; }
-            catch (AccessViolationException) { return default; }
-            catch (InvalidOperationException) { return default; }
+            catch (UnauthorizedAccessException) { throw; }
+            catch (AccessViolationException) { throw; }
+            catch (InvalidOperationException) { throw; }
         }
 
-    }
+        static internal bool CopyDirectory(string directory, string destination, bool overwrite)
+        {
+            // get all files
+            string[] files = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
 
-    public class FileUtils
-    {
-            
+            bool replaceAll  =  default ; 
+
+            foreach (string file in files)
+            {
+                string currentFileDestination = Path.Combine(destination, file);
+                if(File.Exists(currentFileDestination) && overwrite){
+                    if(!replaceAll)
+                    Log.AskFor($"Overwrite existing files? (current:{file})", ()=> replaceAll = true , ()=>  replaceAll =  false) ;
+                    else{
+                        File.Copy(file, currentFileDestination) ;
+                    }
+                }
+            }
+            Log.Error("Something went wrong");
+            return default;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FTM.FileControllers;
+namespace FTM
+{
+    public class FTMCore
+    {
+
+        // entry
+        public static void Main(params string[] args)
+        {
+
+            FileMover.CopyDirectory(args[0], args[1], true);
+
+            static void resume()   => Log.WriteLine("You said yes, lets continue.");
+            static void halt() => Log.WriteLine("You said no, lets go.");
+
+            Log.AskFor("Do you want to continue?", resume, halt);
+        }
+
+    }
+}
