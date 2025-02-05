befd77fc1672f56ceeed48a6431cafeef622232c
Wajid Hussain <65800973+wajid-h@users.noreply.github.com>
2025-02-05 18:08:01 +0500

    Add command attribute

---
+++ src/CommandAttribute.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class CommandAtrribute(
+string callName_, string help_
+): Attribute {
+
+ 
+public readonly string Command 
+{get=> callName_; private set{};
+
+public readonly string Help
+{get=> help_; private set{};}
+
+}
